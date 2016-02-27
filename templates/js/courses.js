cf73f5601f7744eb98d8be2f3fba2cfdf70fd824
Ines <inesmersak@gmail.com>
2016-02-27 23:02:07 +0100

    Lepotni popravki.

--- templates/js/courses.js
+++ templates/js/courses.js
@@ -3,10 +3,5 @@ $(document).ready(function() {
 
     $(".toggle").click(function() {
       $(this).next(".allcourses").toggle();
-      var dec = $(this).css("text-decoration");
-      var newDec;
-      if (dec == "underline") newDec = "none";
-      else newDec = "underline";
-      $(this).css("text-decoration",newDec)
     });
 });
