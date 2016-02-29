0324fe753cdeb117e5e176645c65f7d5c18593ac
Ines <inesmersak@gmail.com>
2016-02-29 13:22:31 +0100

    Popravki po sestanku: zbrisala important, dotted courses, sprememba logota.

--- templates/js/courses.js
+++ templates/js/courses.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     $(".allcourses").hide();
+    $(".toggle").css("border-bottom", "1px dotted #000000");
 
     $(".toggle").click(function() {
       $(this).next(".allcourses").toggle();
+      bord = $(this).css("border-bottom-width");
+      var newBorder;
+      if (bord == "0px") newBorder = "1px dotted #000000";
+      else newBorder = "none";
+      $(this).css("border-bottom", newBorder);
     });
 });
